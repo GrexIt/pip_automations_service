92d1d5c569ec8064abb2b942b5498d57394d0283
ChaluHiver <chalukya@grexit.com>
2020-09-02 17:12:44 +0000

    Adding automation enabled method

---
+++ automationsgetactions/__init__.py
@@ -0,0 +1,11 @@
+import redis
+
+
+def automation_enabled(redis_host):
+    client = redis.Redis(
+        host=redis_host, port=6379, decode_responses=True
+    )
+
+    if not client.keys("sm_id:" + str(sm_id)):
+        return False
+    return True
